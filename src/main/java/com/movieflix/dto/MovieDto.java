2df728c1142d1bbf28b3191ca3ad7426c6264563
Paramas Waewsuwan <paramas_w@tvdirect.tv>
2025-01-23 17:55:22 +0700

    ✨ feat: file upload

---
+++ src/main/java/com/movieflix/dto/MovieDto.java
@@ -0,0 +1,30 @@
+package con.movieflix.dto;
+
+import java.util.Set;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDto {
+    private Integer id;
+
+    @NotBlank(message = "Please provide movie's title!")
+    private String title;
+
+    @NotBlank(message = "Please provide movie's director!")
+    private String director;
+
+    @NotBlank(message = "Please provide movie's studio!")
+    private String studio;
+
+    private Set<String> movieCast;
+
+    private Integer releaseYear;
+
+    @NotBlank(message = "Please provide movie's studio!")
+    private String poster;
+}
